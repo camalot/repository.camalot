9ed76aaf554aa5ae9df5c98cb62ac75a908380bd
camalot <camalot@gmail.com>
2016-11-26 17:03:29 -0600

    only publish gh-pages from master

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,4 +1,4 @@
-if ( $env:APPVEYOR_REPO_BRANCH -eq "master" ) {
+if ( $env:APPVEYOR_REPO_BRANCH -eq "master" -and $ENV:APPVEYOR_PULL_REQUEST_NUMBER -eq "" ) {
   $env:CI_DEPLOY_GITHUB = $true;
 } elseif ( $env:APPVEYOR_REPO_BRANCH -eq "develop" ) {
   $env:CI_DEPLOY_GITHUB_PRE = $true;
