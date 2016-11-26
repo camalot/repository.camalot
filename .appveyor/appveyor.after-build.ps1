9db8db0fa9e80ce5920e243a5c0431403843cbf2
camalot <camalot@gmail.com>
2016-11-26 17:22:11 -0600

    try to fix when it runs gh-publish

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,8 +1,12 @@
-if ( $env:APPVEYOR_REPO_BRANCH -eq "master" -and $ENV:APPVEYOR_PULL_REQUEST_NUMBER -eq "" ) {
-  $env:CI_DEPLOY_GITHUB = $true;
+if ( $env:APPVEYOR_REPO_BRANCH -eq "master" ) {
+  # $env:CI_DEPLOY_GITHUB = $true;
+  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$true", "Machine")
 } elseif ( $env:APPVEYOR_REPO_BRANCH -eq "develop" ) {
-  $env:CI_DEPLOY_GITHUB_PRE = $true;
-  $env:CI_DEPLOY_GITHUB = $false;
+  # $env:CI_DEPLOY_GITHUB_PRE = $true;
+  # $env:CI_DEPLOY_GITHUB = $false;
+  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB_PRE", "$true", "Machine")
+  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$false", "Machine")
 } else {
-  $env:CI_DEPLOY_GITHUB = $false;
+  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB_PRE", "$false", "Machine")
+  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$false", "Machine")
 }
